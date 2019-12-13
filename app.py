ab8bf9c7815617869eb274b598a9e34a0679e2d9
VladaLukovskaya <vladusik.l@yandex.ru>
2019-12-13 09:49:33 +0300

    added hash-passwords and session

--- app.py
+++ app.py
@@ -3,15 +3,26 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 from flask_migrate import Migrate
-from forms import NameForm, LoginForm
+from forms import NameForm, LoginForm, ServiceForm, ApplicationForm, EmployeeForm, NatPersonForm, LegPersonForm, \
+    CountryForm, ForLanKnowForm, CodOfLanForm, CodOfProfLanForm, ClientForm, DocOfClientForm, LogForm, TrusteeForm, \
+    BlankForm, ParticipantFrom
+# from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_required, login_user, UserMixin, LoginManager
 import hashlib
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.filter(User.id == user_id).first()
 
 
 class Role(db.Model):
@@ -20,25 +31,42 @@ class Role(db.Model):
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __str__(self):
-        return 'hello vlada'
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     hash_pass = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
+    # def set_password(self, password):
+    #    self.password_hash = generate_password_hash(password)
+    #    return self.password_hash
+
+    # def check_password(self, password):
+    #    return check_password_hash(self.password_hash, password)
+
+
+Registration = db.Table('registrations',
+                        db.Column('code_of_client', db.Integer, db.ForeignKey('clients.code_of_client')),
+                        db.Column('code_of_country', db.Integer, db.ForeignKey('countries.code_of_country')))
+
+
+Proxy = db.Table('proxies',
+                 db.Column('code_of_client', db.Integer, db.ForeignKey('clients.code_of_client')),
+                 db.Column('num_and_ser_of_doc', db.Integer, db.ForeignKey('trustee.num_and_ser_of_doc')))
+# for nat_per, leg_per and their trustees &&&
+
+
+Log_to_applic = db.Table('log_to_applic',
+                         db.Column('num_of_act', db.Integer, db.ForeignKey('logs.num_of_act')),
+                         db.Column('num_of_applic', db.Integer, db.ForeignKey('applications.num_of_applic')))
+
 
 class Service(db.Model):
-    __tablename__ = 'Services'
+    __tablename__ = 'services'
     code_of_service = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.VARCHAR(30), unique=True)
+    name = db.Column(db.VARCHAR(30), unique=True, nullable=False)
     cost = db.Column(db.Integer)
     addition_docum = db.Column(db.VARCHAR, nullable=True)
     application = db.relationship('Application', lazy='dynamic')
@@ -46,25 +74,25 @@ class Service(db.Model):
 
 
 class Application(db.Model):
-    __tablename__ = 'Applications'
+    __tablename__ = 'applications'
     num_of_applic = db.Column(db.Integer, primary_key=True)
-    code_of_employee = db.Column(db.Integer, db.ForeignKey('Employees.code_of_employee'))  # **
-    code_of_service = db.Column(db.Integer, db.ForeignKey('Services.code_of_service'))  # **
-    date_of_record = db.Column(db.DATE)
-    num_and_ser_of_docum = db.Column(db.Integer, db.ForeignKey('Docums_of_client.num_and_ser_of_doc'))  # **
+    code_of_employee = db.Column(db.Integer, db.ForeignKey('employees.code_of_employee'))  # **
+    code_of_service = db.Column(db.Integer, db.ForeignKey('services.code_of_service'))  # **
+    date_of_record = db.Column(db.DATE, nullable=False)
+    num_and_ser_of_docum = db.Column(db.Integer, db.ForeignKey('docums_of_client.num_and_ser_of_doc'))  # **
     registrarion = db.relationship('Log', secondary=Log_to_applic, lazy='dynamic')  # ???
 
 
 class Employee(db.Model):
-    __tablename__ = 'Employees'
+    __tablename__ = 'employees'
     code_of_employee = db.Column(db.Integer, primary_key=True)
-    surname = db.Column(db.VARCHAR(30))  # фамилия
-    first_name = db.Column(db.VARCHAR(20))  # имя
+    surname = db.Column(db.VARCHAR(30), nullable=False)  # фамилия
+    first_name = db.Column(db.VARCHAR(20), nullable=False)  # имя
     farther_name = db.Column(db.VARCHAR(30), nullable=True)  # отчество
-    address = db.Column(db.VARCHAR(80))
+    address = db.Column(db.VARCHAR(80), nullable=False)
     sex = db.Column(db.Integer)  # 1-male, 2 - female
-    telephone = db.Column(db.VARCHAR(15))
-    type_of_employee = db.Column(db.VARCHAR(20))
+    telephone = db.Column(db.VARCHAR(15), nullable=False)
+    type_of_employee = db.Column(db.VARCHAR(20), nullable=False)
     number_of_licence = db.Column(db.Integer, nullable=True)
     date_of_lic_issuance = db.Column(db.DATE, nullable=True)
     application = db.relationship('Application', lazy='dynamic')
@@ -73,145 +101,125 @@ class Employee(db.Model):
 
 
 class NaturalPerson(db.Model):
-    __tablename__ = 'Natural_Persons'
+    __tablename__ = 'natural_persons'
     code_of_client = db.Column(db.Integer, primary_key=True)
-    surname = db.Column(db.VARCHAR(30))
-    first_name = db.Column(db.VARCHAR(20))
+    surname = db.Column(db.VARCHAR(30), nullable=False)
+    first_name = db.Column(db.VARCHAR(20), nullable=False)
     farther_name = db.Column(db.VARCHAR(30), nullable=True)
     sex = db.Column(db.Integer)  # 1-male, 2 - female
-    date_of_birth = db.Column(db.DATE)
+    date_of_birth = db.Column(db.DATE, nullable=False)
     data_of_marriage = db.Column(db.DATE)  # 1 - in marriage, 2 - not
 
 
-Proxy = db.Table('proxies',
-                 db.Column('code_of_client', db.Integer, db.ForeignKey('Clients.code_of_client')),
-                 db.Column('num_and_ser_of_doc', db.Integer, db.ForeignKey('Trustee.num_and_ser_of_doc')))
-# for nat_per, leg_per and their trustees &&&
-
-
 class LegalPerson(db.Model):
-    __tablename__ = 'Legal_Persons'
+    __tablename__ = 'legal_persons'
     code_of_client = db.Column(db.Integer, primary_key=True)
-    name_of_client = db.Column(db.VARCHAR(50))
+    name_of_client = db.Column(db.VARCHAR(50), nullable=False)
 
 
 class Country(db.Model):
-    __tablename__ = 'Countries'
+    __tablename__ = 'countries'
     code_of_country = db.Column(db.Integer, primary_key=True)
-    name_of_country = db.Column(db.VARCHAR(50))
+    name_of_country = db.Column(db.VARCHAR(50), nullable=False)
 
 
 class ForeignLanKnowledge(db.Model):
-    __tablename__ = 'Foreign_lan_knowledge'
+    __tablename__ = 'foreign_lan_knowledge'
     code_of_lang = db.Column(db.Integer, primary_key=True)
-    code_of_empl = db.Column(db.Integer, db.ForeignKey('Employees.code_of_employee'))  # **
-    code_of_knowl = db.Column(db.Integer, db.ForeignKey('Codif_of_proficiency_lang.code_of_profic'))  # **
+    code_of_empl = db.Column(db.Integer, db.ForeignKey('employees.code_of_employee'))  # **
+    code_of_knowl = db.Column(db.Integer, db.ForeignKey('codif_of_proficiency_lang.code_of_profic'))  # **
 
 
 class CodifOfLanguage(db.Model):
-    __tablename__ = 'Codif_of_language'
+    __tablename__ = 'codif_of_language'
     code_of_lang = db.Column(db.Integer, primary_key=True)
-    name_of_lang = db.Column(db.VARCHAR(15))
+    name_of_lang = db.Column(db.VARCHAR(15), nullable=False)
 
 
 class CodifOfProficiencyLang(db.Model):
-    __tablename__ = 'Codif_of_proficiency_lang'
+    __tablename__ = 'codif_of_proficiency_lang'
     code_of_profic = db.Column(db.Integer, primary_key=True)
-    name_of_profic = db.Column(db.VARCHAR(15))
+    name_of_profic = db.Column(db.VARCHAR(15), nullable=False)
     for_lan_know = db.relationship('ForeignLanKnowledge', lazy='dynamic')
 
 
 class Client(db.Model):
-    __tablename__ = 'Clients'
+    __tablename__ = 'clients'
     code_of_client = db.Column(db.Integer, primary_key=True)
-    code_of_type_of_client = db.Column(db.Integer)  # 1 - legal, 2 - natural
-    code_of_country = db.Column(db.Integer)
+    code_of_type_of_client = db.Column(db.Integer, nullable=False)  # 1 - legal, 2 - natural
     e_mail = db.Column(db.VARCHAR(30))
-    address = db.Column(db.VARCHAR(80))
-    telephone = db.Column(db.VARCHAR(15))
-    code_of_emp = db.Column(db.Integer, db.ForeignKey('Employees.code_of_employee'))  # **
+    address = db.Column(db.VARCHAR(80), nullable=False)
+    telephone = db.Column(db.VARCHAR(15), nullable=False)
+    code_of_emp = db.Column(db.Integer, db.ForeignKey('employees.code_of_employee'))  # **
     doc_of_client = db.relationship('DocumOfClient', lazy='dynamic')
     log = db.relationship('Log', lazy='dynamic')
 
 
-Registration = db.Table('registrations',
-                 db.Column('code_of_client', db.Integer, db.ForeignKey('Clients.code_of_client')),
-                 db.Column('code_of_country', db.Integer, db.ForeignKey('Countries.code_of_country')))
-
-
 class DocumOfClient(db.Model):
-    __tablename__ = 'Docums_of_client'
+    __tablename__ = 'docums_of_client'
     num_and_ser_of_doc = db.Column(db.Integer, primary_key=True)
-    code_of_client = db.Column(db.Integer, db.ForeignKey('Clients.code_of_client'))  # **
-    name_of_doc = db.Column(db.VARCHAR(30))
+    code_of_client = db.Column(db.Integer, db.ForeignKey('clients.code_of_client'))  # **
+    name_of_doc = db.Column(db.VARCHAR(30), nullable=False)
     application = db.relationship('Application', lazy='dynamic')
 
 
 class Log(db.Model):
-    __tablename__ = 'Logs'
+    __tablename__ = 'logs'
     num_of_act = db.Column(db.Integer, primary_key=True)
-    date_of_issuance = db.Column(db.DateTime)
-    code_of_client = db.Column(db.Integer, db.ForeignKey('Clients.code_of_client'))    # **
-    code_of_service = db.Column(db.Integer, db.ForeignKey('Services.code_of_service'))  # **
+    date_of_issuance = db.Column(db.DateTime, nullable=False)
+    code_of_client = db.Column(db.Integer, db.ForeignKey('clients.code_of_client'))    # **
+    code_of_service = db.Column(db.Integer, db.ForeignKey('services.code_of_service'))  # **
     content = db.Column(db.VARCHAR)
     form = db.relationship('Form', lazy='dynamic')
     participant = db.relationship('Participant', lazy='dynamic')
-    registration = db.relationship('Application', secondary=Log_to_applic,  lazy='dynamic')
-
-
-Log_to_applic = db.Table('log_to_applic',
-                 db.Column('num_of_act', db.Integer, db.ForeignKey('Logs.num_of_act')),
-                 db.Column('num_of_applic', db.Integer, db.ForeignKey('Applications.num_of_applic')))
+    dbregistration = db.relationship('Application', secondary=Log_to_applic,  lazy='dynamic')
 
 
 class Trustee(db.Model):
-    __tablename__ = 'Trustee'
+    __tablename__ = 'trustee'
     num_and_ser_of_doc = db.Column(db.Integer, primary_key=True)
-    date_of_regist_of_trust = db.Column(db.DATE)
-    place_of_regist_of_trust = db.Column(db.VARCHAR(30))
+    date_of_regist_of_trust = db.Column(db.DATE, nullable=False)
+    place_of_regist_of_trust = db.Column(db.VARCHAR(30), nullable=False)
 
 
 class Form(db.Model):
-    __tablename__ = 'Forms'
+    __tablename__ = 'forms'
     code_of_form = db.Column(db.Integer, primary_key=True)
-    num_of_form = db.Column(db.Integer)
-    type_of_form = db.Column(db.VARCHAR(40))
-    num_of_not_act = db.Column(db.Integer, db.ForeignKey('Logs.num_of_act'))  # **
+    num_of_form = db.Column(db.Integer, nullable=False)
+    type_of_form = db.Column(db.VARCHAR(40), nullable=False)
+    num_of_not_act = db.Column(db.Integer, db.ForeignKey('logs.num_of_act'))  # **
 
 
 class Participant(db.Model):
-    __tablename__ = 'Participants'
+    __tablename__ = 'participants'
     num_and_ser_of_doc = db.Column(db.Integer, primary_key=True)
-    surname = db.Column(db.VARCHAR(30))
-    first_name = db.Column(db.VARCHAR(20))
+    surname = db.Column(db.VARCHAR(30), nullable=False)
+    first_name = db.Column(db.VARCHAR(20), nullable=False)
     farther_name = db.Column(db.VARCHAR(30), nullable=True)
-    address = db.Column(db.VARCHAR(80))
-    num_of_not_act = db.Column(db.Integer, db.ForeignKey('Logs.num_of_act'))  # **
+    address = db.Column(db.VARCHAR(80), nullable=False)
+    num_of_not_act = db.Column(db.Integer, db.ForeignKey('logs.num_of_act'))  # **
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm(request.form)
-    if request.method == 'POST':
+    if request.method == 'POST' and form.validate_on_submit():
         username = form.username.data
-        password = form.password.data  # need encoding
-        user = User.query.filter_by(username=username).first()
-        if username == User.username:
-            return 'Привет,', User.username
-    return render_template('index.html', block_title='Логин', form=form)
+        password = form.password.data.encode()
+        hash_passw = hashlib.md5(password)
+        user = User.query.filter(User.username == username).first()
+        passw = User.query.filter(User.hash_pass == hash_passw)
+        if user and passw:
+            print('Ouu')
+            login_user(user, remember=form.remember.data)
+            session['username'] = form.username.data
+            # return redirect('/show', 302)
+
+    return render_template('index.html', name=session.get('name'), form=form, known=session.get('known', False))
 
 
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
-
-
-@app.errorhandler(404)
-def page_not_found(er):
-    return render_template('404.html'), 404
-
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
+@login_required
 def index():
     form = NameForm()
     if form.validate_on_submit():
@@ -224,10 +232,107 @@ def index():
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
-@app.route('/show')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    form = ServiceForm(request.form)
+
+
+@app.route('/service', methods=['GET', 'POST'])
+@login_required
+def service():
+    form = ServiceForm(request.form)
+
+
+@app.route('/application', methods=['GET', 'POST'])
+@login_required
+def application():
+    form = ApplicationForm(request.form)
+
+
+@app.route('/employee', methods=['GET', 'POST'])
+@login_required
+def employee():
+    form = EmployeeForm(request.form)
+
+
+@app.route('/natperson', methods=['GET', 'POST'])
+def natperson():
+    form = NatPersonForm(request.form)
+
+
+@app.route('/legperson', methods=['GET', 'POST'])
+def legperson():
+    form = LegPersonForm(request.form)
+
+
+@app.route('/country', methods=['GET', 'POST'])
+def country():
+    form = CountryForm(request.form)
+
+
+@app.route('/lanknow', methods=['GET', 'POST'])
+def lanknow():
+    form = ForLanKnowForm(request.form)
+
+
+@app.route('/codeoflan', methods=['GET', 'POST'])
+def codeoflan():
+    form = CodOfLanForm(request.form)
+
+
+@app.route('/codeofprof', methods=['GET', 'POST'])
+def codeofprof():
+    form = CodOfProfLanForm(request.form)
+
+
+@app.route('/client', methods=['GET', 'POST'])
+def client():
+    form = ClientForm(request.form)
+
+
+@app.route('/docofclient', methods=['GET', 'POST'])
+def docofclient():
+    form = DocOfClientForm(request.form)
+
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    form = LogForm(request.form)
+
+
+@app.route('/trustee', methods=['GET', 'POST'])
+def trustee():
+    form = TrusteeForm(request.form)
+
+
+@app.route('/blank', methods=['GET', 'POST'])
+def blank():
+    form = BlankForm(request.form)
+
+
+@app.route('/partic', methods=['GET', 'POST'])
+def partic():
+    form = ParticipantFrom(request.form)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+@app.errorhandler(404)
+def page_not_found(er):
+    return render_template('404.html'), 404
+
+
+@app.route('/show', methods=['GET', 'POST'])
+@login_required
 def show():
-    user = User()
-    return user.id, user.username
+    form = NameForm
+    user_name = session['user_id']
+    return 'Wow, it is you, ' + user_name + '! Hello'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import StringField, SubmitField, IntegerField, DateField, PasswordField, validators
+from wtforms import StringField, SubmitField, IntegerField, DateField, PasswordField, BooleanField, validators
 from wtforms.validators import DataRequired, InputRequired, Length, ValidationError
 from flask_wtf import FlaskForm
 
@@ -9,52 +9,53 @@ class NameForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', [Length(min=5, max=20), validators.DataRequired])
-    password = PasswordField('Пароль', [Length(min=8, max=30), validators.DataRequired])
+    username = StringField('Логин', [Length(min=5, max=20), validators.DataRequired()])
+    password = PasswordField('Пароль', [Length(min=8, max=30), validators.DataRequired()])
+    remember = BooleanField('Запомнить меня', default=False)
     submit = SubmitField('Войти')
 
 
 class ServiceForm(FlaskForm):
-    name = StringField('Название', validators.DataRequired)
-    cost = IntegerField('Стоимость', validators.DataRequired)
+    name = StringField('Название', validators.DataRequired())
+    cost = IntegerField('Стоимость', validators.DataRequired())
     addition_doc = StringField('Дополнительные документы')
     submit = SubmitField('Ввести')
 
 
 class ApplicationForm(FlaskForm):
-    num_of_applic = IntegerField('Номер заявки', validators.DataRequired)
-    code_of_employee = IntegerField('Код сотрудника', validators.DataRequired)
-    code_of_service = IntegerField('Код услуги', validators.DataRequired)
-    date_of_record = DateField('Дата составления', validators.DataRequired)
-    num_and_ser_of_doc = IntegerField('Номер и серия документа', validators.DataRequired)
+    num_of_applic = IntegerField('Номер заявки', validators.DataRequired())
+    code_of_employee = IntegerField('Код сотрудника', validators.DataRequired())
+    code_of_service = IntegerField('Код услуги', validators.DataRequired())
+    date_of_record = DateField('Дата составления', validators.DataRequired())
+    num_and_ser_of_doc = IntegerField('Номер и серия документа', validators.DataRequired())
     submit = SubmitField('Ввести')
 
 
 class EmployeeForm(FlaskForm):
-    surname = StringField('Фамилия', validators.DataRequired)
-    first_name = StringField('Имя', validators.DataRequired)
+    surname = StringField('Фамилия', validators.DataRequired())
+    first_name = StringField('Имя', validators.DataRequired())
     farther_name = StringField('Отчество')
-    address = StringField('Адрес', validators.DataRequired)
-    sex = IntegerField('Пол', validators.DataRequired)
-    telephone = StringField('Телефон', validators.DataRequired)
-    type_of_employee = StringField('Должность', validators.DataRequired)
+    address = StringField('Адрес', validators.DataRequired())
+    sex = IntegerField('Пол', validators.DataRequired())
+    telephone = StringField('Телефон', validators.DataRequired())
+    type_of_employee = StringField('Должность', validators.DataRequired())
     number_of_licence = IntegerField('Номер лицензии (при наличии)')
     date_of_lic_issuance = DateField('Дата выдачи лицензии (при наличии)')
     submit = SubmitField('Ввести')
 
 
 class NatPersonForm(FlaskForm):
-    surname = StringField('Фамилия', validators.DataRequired)
-    first_name = StringField('Имя', validators.DataRequired)
+    surname = StringField('Фамилия', validators.DataRequired())
+    first_name = StringField('Имя', validators.DataRequired())
     farther_name = StringField('Отчество')
-    sex = IntegerField('Пол', validators.DataRequired)
-    date_of_birth = DateField('Дата рождения', validators.DataRequired)
-    data_of_marriage = DateField('Дата заключения брака', validators.DataRequired)
+    sex = IntegerField('Пол', validators.DataRequired())
+    date_of_birth = DateField('Дата рождения', validators.DataRequired())
+    data_of_marriage = DateField('Дата заключения брака', validators.DataRequired())
     submit = SubmitField('Ввести')
 
 
 class LegPersonForm(FlaskForm):
-    name_of_client = StringField('Название', validators.DataRequired)
+    name_of_client = StringField('Название', validators.DataRequired())
     submit = SubmitField('Ввести')
 
 
@@ -124,8 +125,8 @@ class BlankForm(FlaskForm):
 
 class ParticipantFrom(FlaskForm):
     num_and_ser_of_doc = IntegerField('Номер и серия документа', )
-    surname = StringField('Фамилия', validators.DataRequired)
-    first_name = StringField('Имя', validators.DataRequired)
+    surname = StringField('Фамилия', validators.DataRequired())
+    first_name = StringField('Имя', validators.DataRequired())
     farther_name = StringField('Отчество')
     address = StringField('Адрес', )
     num_of_not_act = IntegerField('Номер нотариального действия', )
